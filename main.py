3d3edf622964d70f198a852fe1cf93c8dd22d6f2
Josh Walker <joshwalker.oni@gmail.com>
2025-04-23 17:32:40 +0100

    Added visual improvements
    Apple and snake got visual improvements, snake looks
    rounded in movement and has cornering segments as well as
    body and head

--- main.py
+++ main.py
@@ -12,13 +12,117 @@ class SNAKE:
         self.direction = Vector2(1, 0)
         self.new_segment = False
 
+        # heads
+        self.head_up = pygame.image.load("Graphics/head_up.png").convert_alpha()
+        self.head_down = pygame.image.load("Graphics/head_down.png").convert_alpha()
+        self.head_right = pygame.image.load("Graphics/head_right.png").convert_alpha()
+        self.head_left = pygame.image.load("Graphics/head_left.png").convert_alpha()
+
+        # tails
+        self.tail_up = pygame.image.load("Graphics/tail_up.png").convert_alpha()
+        self.tail_down = pygame.image.load("Graphics/tail_down.png").convert_alpha()
+        self.tail_right = pygame.image.load("Graphics/tail_right.png").convert_alpha()
+        self.tail_left = pygame.image.load("Graphics/tail_left.png").convert_alpha()
+
+        # body
+        self.body_vertical = pygame.image.load(
+            "Graphics/body_vertical.png"
+        ).convert_alpha()
+        self.body_horizontal = pygame.image.load(
+            "Graphics/body_horizontal.png"
+        ).convert_alpha()
+
+        # curved
+        self.body_tr = pygame.image.load("Graphics/body_tr.png").convert_alpha()
+        self.body_tl = pygame.image.load("Graphics/body_tl.png").convert_alpha()
+        self.body_br = pygame.image.load("Graphics/body_br.png").convert_alpha()
+        self.body_bl = pygame.image.load("Graphics/body_bl.png").convert_alpha()
+
     def draw_snake(self):
+        self.update_head_graphics()
+        self.update_tail_graphics()
+
         # draw each segment
-        for item in self.body:
-            x_pos = int(item.x * cell_size)
-            y_pos = int(item.y * cell_size)
-            segment = pygame.Rect(x_pos, y_pos, cell_size, cell_size)
-            pygame.draw.rect(screen, (0, 0, 111), segment)
+        for index, segment in enumerate(self.body):
+            # rectabgle for positioning
+            x_pos = int(segment.x * cell_size)
+            y_pos = int(segment.y * cell_size)
+            segment_rect = pygame.Rect(x_pos, y_pos, cell_size, cell_size)
+
+            # direction of face
+            if index == 0:
+                screen.blit(self.head, segment_rect)
+
+            elif index == len(self.body) - 1:
+                screen.blit(self.tail, segment_rect)
+
+            else:
+                prev_segment = self.body[index + 1] - segment
+                next_segment = self.body[index - 1] - segment
+
+                # in line segments
+                if prev_segment.x == next_segment.x:
+                    screen.blit(self.body_vertical, segment_rect)
+
+                elif prev_segment.y == next_segment.y:
+                    screen.blit(self.body_horizontal, segment_rect)
+
+                # corners
+                else:
+                    if (
+                        prev_segment.x == -1
+                        and next_segment.y == -1
+                        or prev_segment.y == -1
+                        and next_segment.x == -1
+                    ):
+                        screen.blit(self.body_tl, segment_rect)
+
+                    elif (
+                        prev_segment.x == -1
+                        and next_segment.y == 1
+                        or prev_segment.y == 1
+                        and next_segment.x == -1
+                    ):
+                        screen.blit(self.body_bl, segment_rect)
+
+                    elif (
+                        prev_segment.x == 1
+                        and next_segment.y == -1
+                        or prev_segment.y == -1
+                        and next_segment.x == 1
+                    ):
+                        screen.blit(self.body_tr, segment_rect)
+
+                    elif (
+                        prev_segment.x == 1
+                        and next_segment.y == 1
+                        or prev_segment.y == 1
+                        and next_segment.x == 1
+                    ):
+                        screen.blit(self.body_br, segment_rect)
+
+    def update_head_graphics(self):
+        # see the relative position of second segment to determine which way head faces
+        head_dir = self.body[1] - self.body[0]
+        if head_dir == Vector2(1, 0):
+            self.head = self.head_left
+        elif head_dir == Vector2(-1, 0):
+            self.head = self.head_right
+        elif head_dir == Vector2(0, 1):
+            self.head = self.head_up
+        elif head_dir == Vector2(0, -1):
+            self.head = self.head_down
+
+    def update_tail_graphics(self):
+        tail_dir = self.body[-2] - self.body[-1]
+        if tail_dir == Vector2(1, 0):
+            self.tail = self.tail_left
+        elif tail_dir == Vector2(-1, 0):
+            self.tail = self.tail_right
+        elif tail_dir == Vector2(0, 1):
+            self.tail = self.tail_up
+        elif tail_dir == Vector2(0, -1):
+            self.tail = self.tail_down
 
     def move_snake(self):
         if self.new_segment:
@@ -48,7 +152,8 @@ class FRUIT:
             cell_size,
         )
         # draw rectangle
-        pygame.draw.rect(screen, (126, 166, 114), fruit_rect)
+        # pygame.draw.rect(screen, (126, 166, 114), fruit_rect)
+        screen.blit(apple, fruit_rect)
 
     def random_pos(self):
         self.x = random.randint(0, cell_number - 1)
@@ -108,8 +213,12 @@ screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_si
 # Clock, keep the speed consistent
 clock = pygame.time.Clock()
 
+# load images
+apple = pygame.image.load("Graphics/apple.png").convert_alpha()
+
+# event timer
 SCREEN_UPDATE = pygame.USEREVENT
-pygame.time.set_timer(SCREEN_UPDATE, 100)
+pygame.time.set_timer(SCREEN_UPDATE, 120)
 
 main_game = MAIN()
 
